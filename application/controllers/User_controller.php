8dc72ad7b41923dbbdd275c23a84b04e8df73d3d
yogamandayu <yoga.grahadi@gmail.com>
2018-11-05 09:48:28 +0700

    [yogamandayu] fix showing profile picture

--- application/controllers/User_controller.php
+++ application/controllers/User_controller.php
@@ -8,6 +8,14 @@ class User_controller extends CI_Controller{
     $this->load->helper('url');
     $this->load->library('session');
     $this->load->model('User_model');
+    $this->load->model('Admin_model');
+    require 'cloudinary/autoload.php';
+    require 'cloudinary/src/Helpers.php';
+    \Cloudinary::config(array(
+      "cloud_name" => "yogamandayu",
+      "api_key" => "722227313554865",
+      "api_secret" => "9jxvGCG9Tpfeu4EQhdSbxraRXF8"
+    ));
     if($this->session->userdata('status') != "login"){
 			redirect(base_url(''));
 		}
@@ -28,8 +36,26 @@ class User_controller extends CI_Controller{
   public function show(){
     $data = $this->User_model->select($this->User_model->select_id($this->session->userdata('username')));
     #echo var_dump($data);
+    $this->check_debt($data->id_user);
+    $data->foto = cl_image_tag($data->image, array("width"=>200, "crop"=>"scale", "alt" => "Foto Profil" ));
     $this->load->view('user/view_show',$data);
   }
-}
 
- ?>
+  public function check_debt($id){
+    $data['debt_start'] = $this->Admin_model->select($id)->row('debt_start');
+    $data['debt_end'] = $this->Admin_model->select($id)->row('debt_end');
+    if(date('Y-m-d') > $data['debt_end']){
+      $time = strtotime($data['debt_end']);
+      $data['debt_end'] = date('Y-m-10', strtotime('+1 month', $time));
+
+      $awal = new DateTime($data['debt_start']);
+      $akhir = new DateTime($data['debt_end']);
+      $diff = $akhir->diff($awal);
+
+      $data["debt_month"] = ($diff->format('%y') * 12) + $diff->format('%m');
+      $data["id_status"] = 0;
+      $this->Admin_model->update_account($id,$data);
+    }
+  }
+}
+?>
